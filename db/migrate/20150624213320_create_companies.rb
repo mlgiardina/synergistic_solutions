baa7aded0b316d820a92c04709e0f631ee0b1b46
Matthew Giardina <mlgiardina@me.com>
2015-06-24 16:46:27 -0500

    removed 'has_many' from migrations

--- db/migrate/20150624213320_create_companies.rb
+++ db/migrate/20150624213320_create_companies.rb
@@ -1,7 +1,6 @@
 class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
-      t.has_many :locations
       t.string :name
 
       t.timestamps null: false
--- db/migrate/20150624213350_create_locations.rb
+++ db/migrate/20150624213350_create_locations.rb
@@ -1,8 +1,6 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
-      t.has_many :employees
-      t.has_many :courses
       t.belongs_to :company, index: true, foreign_key: true, null: false
       t.string :city
 
